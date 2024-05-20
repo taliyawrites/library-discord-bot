cc9ab524be4e4fc64564d1fa66a87957adeafd8f
taliyawrites <tal528491@gmail.com>
2024-05-20 12:59:52 -0500

    some edits

--- trialBot.py
+++ trialBot.py
@@ -19,8 +19,8 @@ OPTIONS_ROLE = int(os.getenv('ROLE_ID_OPTIONS'))
 WINNER_ROLE = int(os.getenv('ROLE_ID_WINNER'))
 airtable_api = Api(os.getenv('AIRTABLE_TOKEN'))
 
-RECENT_WINNER_TOLERANCE = 2
-RECENT_AUDIO_TOLERANCE = 90
+REPEAT_WINNER_TOLERANCE = 2
+REPEAT_AUDIO_TOLERANCE = 90
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -31,7 +31,12 @@ client = discord.Client(intents=intents)
 
 
 
-test_time = datetime.time(hour=23, minute=8)
+
+test_time = datetime.time(hour=14, minute=0) # in utc 
+new_winner_time, daily_audio_time, daily_balatro_time = test_time, test_time, test_time
+# new_winner_time = datetime.time(hour=14, minute=0)
+# daily_audio_time = datetime.time(hour=15, minute=0)
+# daily_balatro_time = datetime.time(hour=8, minute=0)
 
 
 
@@ -137,9 +142,8 @@ def random_audio(audios, tag=None):
 
 def import_airtable_data():
     table = airtable_api.table('apprrNWlCwDHYj4wW', 'tblqwSpe5CdMuWHW6')
-    all_records = table.all()
-    all_audios = [Audio(entry) for entry in all_records]
-    
+    all_audios = [Audio(entry) for entry in table.all()]
+
     allowed = []
     for audio in all_audios:
         if audio.allowed_choice():
@@ -158,11 +162,9 @@ def import_airtable_data():
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-
     # import data from airtable
     global audio_choices
     audio_choices = import_airtable_data()
-
 
     # set all daily tasks running
     choose_winner.start()
@@ -222,18 +224,30 @@ async def on_message(message):
         await message.channel.send(f"the Balatro seed of the day is: {random_seed}")
 
     if message.content.startswith('!schedule'):
-        schedule = "Sunday 4:30PM EST: Private Library Release \n Monday 4:30PM EST: Reddit GWA Release \n Wednesday 6:30PM EST: Library Card Release \n Every other Thursday 4:30PM EST: Reddit GWA Release \n Friday 6:30PM EST: Book Club Release"
+        # schedule = "Sunday 4:30PM EST: Private Library Release \n Monday 4:30PM EST: Reddit GWA Release \n Wednesday 6:30PM EST: Library Card Release \n Every other Thursday 4:30PM EST: Reddit GWA Release \n Friday 6:30PM EST: Book Club Release"
+        schedule = "Sunday 4:30PM EST (<t:1716755400:t>): Private Library Release \n Monday 4:30PM EST (<t:1716841800:t>): Reddit GWA Release \n Wednesday 6:30PM EST (<t:1717021800:t>): Library Card Release \n Every other Thursday 4:30PM EST (<t:1717101000:t>): Reddit GWA Release \n Friday 6:30PM EST (<t:1717194600:t>): Book Club Release"
         schedule_embed = discord.Embed(title = "Vel's Posting Schedule",description=schedule)
         await message.channel.send(embed=schedule_embed)
 
     if message.content.startswith('!live'):
-        await message.channel.send("Vel does a live recordings here on discord every Sunday at 7:30PM EST.")
+        await message.channel.send("Vel does live audio recordings here on discord every Sunday at 7:30PM EST (<t:1716766200:t>)!")
 
     if message.content.startswith('!social'):
-        await message.channel.send("here are links to all of Vel's socials")
+        # await message.channel.send("here are links to all of Vel's socials")
         links = "[twitter](https://x.com/VelsLibrary) \n [reddit](https://www.reddit.com/user/VelsLibrary/) \n [twitch](https://www.twitch.tv/velslibrary) \n [pornhub](https://www.pornhub.com/model/velslibrary) \n [youtube](https://www.youtube.com/@VelsLibrary)"
         link_embed = discord.Embed(title = "Vel's Socials",description=links)
         await message.channel.send(embed=link_embed)
+
+
+    # if message.content.startswith('!getrole'):
+    #     msg = await message.channel.send("React to this message to get the 'I wanna be a good girl' role!")
+    #     try:
+    #         reaction, user = await client.wait_for('reaction_add', timeout = 20)
+    #     except asyncio.TimeoutError:
+    #         await message.channel.send('timeout')
+    #     else:
+    #         await message.channel.send(reaction.emoji)
+
 
 
 
@@ -248,10 +262,8 @@ def choose_next(options, recent):
     recent.pop(0)
     return next
 
-#in utc
-# new_winner_time = datetime.time(hour=14, minute=0)
-new_winner_time = test_time
-recent_winners = [None for i in range(RECENT_WINNER_TOLERANCE)]
+
+recent_winners = [None for i in range(REPEAT_WINNER_TOLERANCE)]
 
 
 @tasks.loop(time = new_winner_time)
@@ -260,13 +272,13 @@ async def choose_winner():
     channel = client.get_channel(GENERAL)
     good_girl_role = guild.get_role(WINNER_ROLE)
 
+    await asyncio.sleep(5)
     for member in good_girl_role.members:
         await member.remove_roles(good_girl_role)
 
     options = guild.get_role(OPTIONS_ROLE).members
     winner = choose_next(options, recent_winners)
 
-    await asyncio.sleep(5)
     await channel.send(f'{winner.mention} is the good girl of the day!')
     await winner.add_roles(good_girl_role)
 
@@ -283,10 +295,8 @@ def choose_next_audio(options, recent):
     return next
 
 
-# daily_audio_time = datetime.time(hour=15, minute=0)
-daily_audio_time = test_time
-recent_audios = ['' for i in range(RECENT_AUDIO_TOLERANCE)]
 
+recent_audios = ['' for i in range(REPEAT_AUDIO_TOLERANCE)]
 
 @tasks.loop(time = daily_audio_time)
 async def choose_daily_audio():
@@ -303,14 +313,10 @@ async def choose_daily_audio():
 
 
 
-# daily_balatro_time = datetime.time(hour=8, minute=0)
-daily_balatro_time = test_time
-
 @tasks.loop(time=daily_balatro_time)
 async def daily_balatro():
     global random_seed
-    random_seed = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits, k=8))
+    random_seed = ''.join(random.choices(string.ascii_uppercase+string.digits, k=8))
 
 
 

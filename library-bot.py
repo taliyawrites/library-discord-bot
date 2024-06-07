baede8490df5ced298849a34dc6607010ed9c0cc
taliyawrites <tal528491@gmail.com>
2024-06-07 16:55:18 -0500

    testing

--- library-bot.py
+++ library-bot.py
@@ -23,7 +23,7 @@ WINNERS_FILENAME = "recentwinners.txt"
 AUDIOS_FILENAME = "recentaudios.txt"
 
 HOUR = 22
-MINUTE = 49
+MINUTE = 56
 
 intents = discord.Intents.default()
 intents.message_content = True

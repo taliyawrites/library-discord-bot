944857ebd983db012a1d031e070672e8417bd165
taliyawrites <tal528491@gmail.com>
2024-08-08 19:50:54 -0500

    updated repeat tolerance

--- library-bot.py
+++ library-bot.py
@@ -450,26 +450,6 @@ async def on_message(message):
         audio_choices = import_airtable_data()
         await message.author.send("Masterlist data sync'ed with Airtable updates.")
 
-    if msg.startswith('!testdaily'):
-        guild = client.get_guild(GUILD)
-        good_girl_role = guild.get_role(WINNER_ROLE)
-        options = guild.get_role(OPTIONS_ROLE).members
-        recent = read_from_file(WINNERS_FILENAME)
-        choices = []
-
-        for audio in options:
-            if audio.name not in recent:
-                choices.append(audio)
-
-        if len(choices) != 0:
-            next_one = random.choice(choices)
-        else:
-            print("no viable options; choosing random")
-            next_one = random.choice(choices)
-
-        await message.author.send(next_one.name)
-
-
 
 # DAILY LOOPING TASKS
 

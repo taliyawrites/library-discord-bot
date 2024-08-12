8548e107d099129157dd478c7c7f2666a39747c2
taliyawrites <tal528491@gmail.com>
2024-08-12 13:16:04 -0500

    add some more error exits

--- library-bot.py
+++ library-bot.py
@@ -346,10 +346,17 @@ async def on_message(message):
 
 
     if msg.startswith('!title'):
-        phrase = msg[7:]
+        phrase = msg[7:].strip()
+
+        if len(phrase) == 0:
+            await message.channel.send("Please enter a search phrase after `!title `.")
+            return
+
         if phrase[0] == '"' or phrase[0] == "'":
             phrase = phrase[1:-1]
         phrase = phrase.strip()
+
+
         matches = title_matches(phrase)
 
         if len(matches) == 0:
@@ -371,6 +378,11 @@ async def on_message(message):
 
     if msg.startswith('!character'):
         name = msg[11:].strip()
+
+        if len(name) == 0:
+            await message.channel.send("Please specify a character name.")
+            return
+
         matches = character_search(name)
 
         if len(matches) == 0:

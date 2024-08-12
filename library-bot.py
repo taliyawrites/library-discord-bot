711c421d0b56dda84eced77dae88869a4583eaf7
taliyawrites <tal528491@gmail.com>
2024-08-11 23:47:07 -0500

    DM all errors with traceback

--- library-bot.py
+++ library-bot.py
@@ -650,7 +650,10 @@ async def on_member_join(member):
 @client.event
 async def on_error(event, *args, **kwargs):
     message = args[0]
-    await taliya.send("**ERROR:** " + message.jump_url + "\n**MESSAGE CONTENT:** " + message.content + "\n\n" + traceback.format_exc())
+    if not isinstance(message.channel, discord.DMChannel):
+        await taliya.send("**ERROR:** " + message.jump_url + "\n**MESSAGE CONTENT:** " + message.content + "\n\n" + traceback.format_exc())
+    else:
+        await taliya.send("**ERROR:** " + message.jump_url + "\n**MESSAGE CONTENT:** " + message.content + "\n\n" + traceback.format_exc())
 
 
 

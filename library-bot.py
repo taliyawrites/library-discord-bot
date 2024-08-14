37e42b2ddb6f6163a3ffd3da9fed650b2b3469fd
taliyawrites <tal528491@gmail.com>
2024-08-14 14:56:59 -0500

    private update

--- library-bot.py
+++ library-bot.py
@@ -290,7 +290,7 @@ async def setup_hook():
 
     global taliya
     taliya = await client.fetch_user(1169014359842885726)
-    await taliya.send("Card Catalog bot restarted successfully!")
+    await taliya.send("Card Catalog bot has been restarted successfully!")
 
 
 

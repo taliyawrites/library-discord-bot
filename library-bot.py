a358355fc6f2059915de3c1cd82311b2327e4ae9
taliyawrites <tal528491@gmail.com>
2024-08-27 12:17:39 -0500

    vel idea

--- library-bot.py
+++ library-bot.py
@@ -518,18 +518,18 @@ async def on_message(message):
             await message.channel.send("What? Are you really so horny that you thought there would be some special message for 69? Sluts like you are so predictable, you know. So needy and desperate and completely at the mercy of your pathetic fucking cunt. But you like being that way, don't you? Silly whore.")
 
 
-        if msg.startswith('!degrade'):
-            adjectives = ["desperate","pretty","depraved","pathetic","needy","worthless"]
-            nouns = ["whore","slut","bitch","cunt","set of holes"]
-            response = random.choice(adjectives) + " " + random.choice(nouns) + "."
-            await message.channel.send(response)
+    if msg.startswith('!degrade'):
+        adjectives = ["desperate","pretty","depraved","pathetic","needy","worthless"]
+        nouns = ["whore","slut","bitch","cunt","set of holes"]
+        response = random.choice(adjectives) + " " + random.choice(nouns) + "."
+        await message.channel.send(response)
 
 
-        if msg.startswith('!praise'):
-            adjectives = ["perfect","pretty","beautiful"]
-            nouns = ["angel","bunny","pet","princess","toy"]
-            response = random.choice(adjectives) + " " + random.choice(nouns) + "!"
-            await message.channel.send(response)
+    if msg.startswith('!praise'):
+        adjectives = ["perfect","pretty","beautiful"]
+        nouns = ["angel","bunny","pet","princess","toy"]
+        response = random.choice(adjectives) + " " + random.choice(nouns) + "!"
+        await message.channel.send(response)
 
 
 

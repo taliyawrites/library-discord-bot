ef31c753d3e495cd4932aecadcb7a1d27f660c73
taliyawrites <tal528491@gmail.com>
2024-09-08 16:41:40 -0500

    permissions without repalcement

--- library-bot.py
+++ library-bot.py
@@ -835,7 +835,7 @@ async def choose_good_girl():
         winners = random.sample(options, 5)
         global cum_permission_ids
         cum_permission_ids  = [user.id for user in winners]
-        print(f"daily permissions assigned to: {winners[0].display_name}, {winners[1].display_name}, {winners[2].display_name}, and {winners[3].display_name}")
+        print(f"daily permissions assigned to: {winners[0].display_name}, {winners[1].display_name}, {winners[2].display_name}, {winners[3].display_name}, and {winners[4].display_name}")
         save_to_file(RECORD_FILENAME,[str(ids) for ids in cum_permission_ids])
 
 

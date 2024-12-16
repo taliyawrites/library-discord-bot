23bdf65afa33f6ba1a55a10f3eaa99e9403a25bf
taliyawrites <tal528491@gmail.com>
2024-12-16 17:47:25 -0600

    threads command

--- library-bot.py
+++ library-bot.py
@@ -874,6 +874,13 @@ async def on_message(message):
             link_string = link_string + "- " + thread.jump_url + "\n"
         await message.channel.send(link_string)
 
+    if msg.startswith('!altthread'):
+        threads = await client.get_guild(GUILD).threads
+        link_string = ""
+        for thread in threads:
+            link_string = link_string + "- " + thread.jump_url + "\n"
+        await message.channel.send(link_string)
+
 
 
     # SILLY COMMANDS #

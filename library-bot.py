8046ecb6b22f25dac5d7ed97037d7ea2b61527f8
taliyawrites <tal528491@gmail.com>
2025-02-03 20:57:45 -0600

    rewrite tutorial

--- library-bot.py
+++ library-bot.py
@@ -409,7 +409,7 @@ async def randomaudio(interaction, taglist: Optional[str] = None):
 @app_commands.rename(title_phrase = "title")
 @app_commands.describe(title_phrase="any words or phrases from the audio's title that you remember")
 async def title(interaction, title_phrase: str):
-    phrase = title_phrase.lower().replace("’","'").replace('“','"').replace('”','"').strip()
+    phrase = title_phrase.lower().replace("’","'").replace('“','"').replace('”','"').replace("‘","'").strip()
     if phrase[0] == '"' or phrase[0] == "'":
         phrase = phrase[1:-1]
     phrase = phrase.strip()

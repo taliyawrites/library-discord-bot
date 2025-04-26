5b0b7e563dbc5b06bbe98a04a9693d8f95fe8ab6
taliyawrites <tal528491@gmail.com>
2025-04-26 12:24:55 -0500

    add autocomplete for character names

--- library-bot.py
+++ library-bot.py
@@ -352,7 +352,7 @@ async def on_ready():
 async def setup_hook():
     print("setup hook running")
     await tree.sync()
-    command_guild = client.fetch_guild(COMMAND_SERVER)
+    command_guild = await client.fetch_guild(COMMAND_SERVER)
     print(command_guild.name)
     print("try")
     await tree.sync(guild = command_guild)

5a33ba25e14825bff4edd8d8224ae91c3b536fb7
taliyawrites <tal528491@gmail.com>
2025-04-26 12:15:27 -0500

    add autocomplete for character names

--- library-bot.py
+++ library-bot.py
@@ -352,9 +352,6 @@ async def on_ready():
 async def setup_hook():
     print("setup hook running")
     await tree.sync()
-    await tree.sync(guild = client.get_guild(COMMAND_SERVER))
-
-    print("synced")
 
     # import data from airtable
     global audio_choices, tag_dictionary, collections

6cab97a08a18c906dffe4569c4e4abb4d05e90f9
taliyawrites <tal528491@gmail.com>
2025-06-15 23:29:17 -0700

    updated twitch week schedule

--- library-bot.py
+++ library-bot.py
@@ -956,7 +956,7 @@ async def stream(interaction):
     stream_embed = discord.Embed(title = "Vel's Livestreams", description = stream_info, url = "https://www.twitch.tv/velslibrary")
     file = discord.File("schedule.webp", filename="schedule.webp")
     stream_embed.set_image(url="attachment://schedule.webp")
-    await interaction.followup.send(embed=stream_embed)
+    await interaction.followup.send(file = file, embed=stream_embed)
 
 
 @tree.command(name = "merch", description = "Information about Vel's merch!")

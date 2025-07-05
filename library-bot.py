a8c22dd0b0873e7ed93abdebdb715da11f5d524e
taliyawrites <tal528491@gmail.com>
2025-07-05 15:59:06 -0700

    violence

--- library-bot.py
+++ library-bot.py
@@ -1279,7 +1279,10 @@ async def birthdayremove(interaction):
 
 
 
-
+@tree.command(name = "tierlist", description = "Links to Vel's official tier list of all his audios!")
+async def count(interaction):
+    await interaction.response.defer()
+    await interaction.followup.send("You can see Vel's official tier list of all his audios here! https://discord.com/channels/1148449914188218399/1194499430410371173/1391176460781359224 This was done live on twitch stream (use `/stream` for  more details on how to join us next time). If you'd like to do your own tierlist, you can [make your own here](https://tiermaker.com/create/vels-library-audio-tierlist-18367623-2), courtesy of Kayla! ")
 
 
 

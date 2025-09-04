bf510595b5cb77ba4306de762a1d9da77dddf786
taliyawrites <tal528491@gmail.com>
2025-09-04 10:18:41 -0700

    add pt and ep to ignored words list

--- library-bot.py
+++ library-bot.py
@@ -259,7 +259,7 @@ def inexact_matches(phrase):
     matching = []
     closer_matches = []
     search_terms = phrase.split(" ")
-    too_common_words = ["the","a","an","is","on","for","you","my","i","to","me","up","and","are","with","your","by","part","of"]
+    too_common_words = ["the","a","an","is","on","for","you","my","i","to","me","up","and","are","with","your","by","part","of","pt","pt.","ep","ep."]
     search_words = []
     for word in search_terms:
         if word not in too_common_words and (len(word) > 2 or word.isnumeric()):

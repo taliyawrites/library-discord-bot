981ab5d15a00e8843e30442cd615cf54a3bab0fe
taliyawrites <tal528491@gmail.com>
2025-09-04 15:09:10 -0700

    mod_ids

--- library-bot.py
+++ library-bot.py
@@ -478,8 +478,8 @@ async def setup_hook():
     global taliya, vel, mod_ids
     taliya = await client.fetch_user(1169014359842885726)
     vel = await client.fetch_user(1089053035377999912)
-    # mod_ids = [mod.id for mod in client.get_guild(GUILD).get_role(1239743183617790015).members]
-    # print(mod_ids)
+    mod_ids = [mod.id for mod in await client.fetch_guild(GUILD).get_role(1239743183617790015).members]
+    print(mod_ids)
 
     await taliya.send(f"Card Catalog bot restarted successfully!")
     print(f"bot local time: {datetime.datetime.now().hour}h{datetime.datetime.now().minute}.")

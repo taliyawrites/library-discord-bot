ba44833aae216a167899a5b6b3fbb26103b3f872
taliyawrites <tal528491@gmail.com>
2025-11-04 13:55:49 -0800

    new on member update

--- library-bot.py
+++ library-bot.py
@@ -1856,7 +1856,7 @@ async def on_member_update(before, after):
                 now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
                 file.write(f"[{now}] Patreon membership renewed for User {after.id} ({after.name}). Roles updated from {[role.name for role in before.roles]} to {[role.name for role in after.roles]} \n")
 
-            await after.send("Welcome back to the Vel's Library discord server! You can customize the channels you want to see here <id:browse>! If you're having trouble or still missing channels, please submit a ticket through https://discord.com/channels/1148449914188218399/1192558831222411294 and we'll help you access them!")
+            await after.send("Welcome back to the Vel's Library discord server! You can customize the channels you want to see and re-enter yourself as eligible for Good Girl of the Day here <id:customize>! If you're having trouble or still missing channels, please submit a ticket through https://discord.com/channels/1148449914188218399/1192558831222411294 and we'll help you access them!")
             print(f"welcome back message sent to {after.name}")
 
 

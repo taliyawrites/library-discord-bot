9e5c4d7d29af489c7726aa5dcc3ef60a9dc0a592
taliyawrites <tal528491@gmail.com>
2025-11-07 16:04:43 -0800

    partial tag matches autocomplete

--- library-bot.py
+++ library-bot.py
@@ -561,6 +561,15 @@ async def randomaudio(interaction, taglist: Optional[str] = None):
         audio = random_audio(audio_choices)
         # await interaction.followup.send(f"Here's a random audio!")
         await interaction.followup.send(embed = audio.discord_post())
+@randomaudio.autocomplete('taglist')
+async def randomaudio_autocomplete(interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
+    canonical_matches = [app_commands.Choice(name=opt, value=opt) for opt in sorted_tag_list if current.lower() in opt.lower()][:25]
+    if len(canonical_matches) > 24:
+        return canonical_matches
+    else:
+        partials = list(set([app_commands.Choice(name=tag_dictionary[opt], value=tag_dictionary[opt]) for opt in all_tags if current.lower() in opt.lower()][:25]))
+        return list(set((partials + canonical_matches)))[:25]
+
 
 
 

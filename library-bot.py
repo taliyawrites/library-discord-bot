815530a3001e539be59533000e760c24fb4c86d4
taliyawrites <tal528491@gmail.com>
2026-05-05 16:51:55 -0700

    automating further tags needed

--- library-bot.py
+++ library-bot.py
@@ -2073,6 +2073,7 @@ def further(tags, warnings):
         return False
     elif len(warnings) > 0:
         warning_list = warnings.split("\n")
+        print(warning_list)
         if len(warning_list) == 1 and "power dynamics" in warning_list[0]:
             return False
         else:

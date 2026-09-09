15151c539c2df1d2e19cc58b5a1f208f3f557020
taliyawrites <tal528491@gmail.com>
2026-09-09 01:15:35 -0700

    secret hammers command

--- library-bot.py
+++ library-bot.py
@@ -2822,8 +2822,8 @@ async def upcoming_audios(mentionQ, at, channel, boldQ):
                 date_info = f"{fields.get("Day","Day of Week")}, {date_format(fields["Due Date"])}: "
                 platform = fields.get("Platform","")
                 if len(platform) != 0:
-                    platform = "[" + platform + "]"
-                audio_name =  "*" + fields.get("Audio Name", "Unnamed Audio")  + "*"
+                    platform = " [" + platform + "]"
+                audio_name =  fields.get("Audio Name", "Unnamed Audio")
                 script = fields.get("Script Link","")
                 if len(script) != 0:
                     audio_name += f" ([script]({fields["Script Link"]}) by {fields["Scriptwriter"]})"
